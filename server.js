45b7bc3c17a05af87e954a5ae71ec92e0299fd30
Yogeshwaran K <yogeshwaran.k2021c@vitstudent.ac.in>
2025-09-08 11:36:50 +0530

    fixed cors

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://web-bot-frontend.vercel.app', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 // Routes
