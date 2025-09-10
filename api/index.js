c46c358f2aae34375d98a3ad4bdbaa28f22f98d5
Yogeshwaran K <yogeshwaran.k2021c@vitstudent.ac.in>
2025-09-10 11:13:57 +0530

    Fix CORS credentials issue

--- api/index.js
+++ api/index.js
@@ -5,10 +5,9 @@ const app = express();
 
 // Manual CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://web-bot-frontend.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
